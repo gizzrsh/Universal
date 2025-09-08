610b30a27d291ab1c988c1324ff3f9bdecfd9c90
Gizzyaka <gizzatullint@bk.ru>
2025-09-09 02:00:14 +0500

    add section "selection"

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ let items = document.querySelectorAll('.recommended__item');
 
 // Перебираю массив items
 items.forEach(item => {
+    // Добавляю действие при клике на item
     item.addEventListener('click', () => {
         
+        // Перебираю массив items и у всех item удаляю класс с атрибутом --active
         items.forEach(item => item.classList.remove('recommended__item--active'));
         
+        // Добавляю при клике класс с атрибутом --active
         item.classList.add('recommended__item--active');
     })
 })
